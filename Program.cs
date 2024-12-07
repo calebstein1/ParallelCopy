73976428b77880c34f1c4e55faecf124b1fbc90d
Caleb Stein <caleb@calebstein.net>
2024-12-06 16:39:46 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ParallelCopy
+{
+    class Program
+    {
+        private static List<Task> _copies = [];
+
+        internal static async Task<int> Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Must provide at least 2 arguments");
+                return -1;
+            }
+            
+            var outPath = args[^1];
+
+            for (var i = 0; i < args.Length - 1; i++)
+            {
+                var inName = Path.GetFileName(args[i]);
+                var inStream = new FileStream(args[i], FileMode.Open);
+                var outStream = new FileStream(Path.Combine(outPath, inName), FileMode.OpenOrCreate);
+                
+                Console.WriteLine($"Copying {args[i]} to {outPath}");
+                _copies.Add(inStream.CopyToAsync(outStream));
+            }
+            
+            await Task.WhenAll(_copies);
+            Console.WriteLine("Done!");
+            return 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# ParallelCopy
+
+This is a tiny parallel copy utility I threw together while waiting for a bunch of large sequential copies to complete.
+I got this done before the sequential copies finished, finished my copies with it, and then figured it may be useful to others... maybe.
+
+## Building
+
+Super simple, just a `dotnet build` will get you there.
+
+## Usage
+
+```Bash
+ParallelCopy [files] <destination>
+```
+
